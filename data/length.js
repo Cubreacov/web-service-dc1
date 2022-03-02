1cef86966211ab439bdfe5c95e5bab7c524459b8
Daniela Cubreacov <91621633+Cubreacov@users.noreply.github.com>
2022-03-02 22:40:23 +0000

    Ads personal data

---
+++ data/length.js
@@ -0,0 +1,39 @@
+{
+	"title": "BTS albums by length",
+	
+	"albums":[
+             {
+				"Album":3,
+				"Title":"Dark & Wild",
+				"Year":2014,
+				"Language":"Korean",
+				"Length":"51:32",
+				"Genre":"Hip hop; R&B;dance-pop; Pop",
+				"Producers":"Pdogg, SUGA, Supreme Boi,Shawn",
+        "Image":"pic.jpg"
+               
+			  },
+        {
+				"Album":2,
+				"Title":"Skool Luv Affair",
+				"Year":2014,
+				"Language":"Korean",
+				"Length":"35:30",
+				"Genre":"Hip hop; R&B; dance-pop; Pop",
+				"Producers":"Pdogg",
+        "Image":"pic.jpg"
+			  },
+        {
+				"Album":1,
+				"Title":"O!RUL8,2?",
+				"Year":2013,
+				"Language":"Korean",
+				"Length":"30:36",
+				"Genre":"Hip hop; R&B",
+				"Producers":"Pdogg, Hitman Bang, Supreme Boi",
+        "Image":"pic.jpg"
+			  }
+			  
+
+			]
+}
--- index.php
+++ index.php
@@ -1,22 +1,99 @@
 <html>
-    <head>
-        <title>PHP File!</title>
-    </head>
+<head>
+<title>BTS  Web Service Demo</title>
+<style>
+      body {font-family:georgia;}
 
-<body>
+  .albums{
+    border:1px solid #E77DC2;
+    border-radius: 5px;
+    padding: 5px;
+    margin-bottom:5px;
+    position:relative;   
+  }
+ 
+  .pic{
+    position:absolute;
+    right:10px;
+    top:10px;
+  }
+  .pic{
+    max-width:80px;
+  }
 
-    <h1>PHP File!</h1>
+</style>
+<script src="https://code.jquery.com/jquery-latest.js" type="text/javascript"></script>
 
-    <p>This is HTML</p>
+<script type="text/javascript">
 
-        <?php
-    echo "<p> This is PHP!</p>";
-        ?> 
-    <p>This is HTML</p>
-    <p>Hello from replit</p>
+ function btsTemplate(albums){
+  return `<div class = "albums">
+    <b> Album: </b> ${albums.Album} <br />
+    <b> Title: </b>  ${albums.Title} <br />
+    <b> Year: </b>  ${albums.Year} <br />
+    <b> Language: </b>  ${albums.Language} <br />
+    <b> Length: </b>  ${albums.Length} <br />
+    <b> Genre: </b>  ${albums.Genre} <br />
+    <b> Producers: </b>  ${albums.Producers} <br />
+     <div class = "pic"> <img src ="thumbnails/${albums.Image}"/></div>
 
-</body>
+  </div>`;
+}
 
 
+$(document).ready(function() { 
+ 
+ $('.category').click(function(e){
+   e.preventDefault(); //stop default action of the link
+   cat = $(this).attr("href");  //get category from URL
+  
+   
+   var request = $.ajax({
+     url: "api.php?cat=" + cat,
+     method: "GET",
+     dataType: "json"
+   });
+   request.done(function( data ) {
+     console.log(data);
+      //place the title on the page
+     $("#albums").html(data.albums);
+     //clear previous films
+     $("#albums").html("");
+      //load each film via template into div
+     $.each(data.albums, function(key, value){
+       let str = btsTemplate(value);
+       $("<div></div>").html(str).appendTo("#albums");
+      });
 
+     });
+
+     
+     
+    
+   
+  request.fail(function(xhr, status, error) {
+               //Ajax request failed.
+             var errorMessage = xhr.status + ': ' + xhr.statusText
+             alert('Error - ' + errorMessage);
+          });
+  });
+  });
+  
+
+  
+</script>
+</head>
+	<body>
+	<h1>BTS Albums Web Service</h1>
+		<a href="year" class="category">BTS Albums By Year</a><br />
+		<a href="length" class="category">BTS Albums By Length</a>
+		<h3 id="albumstitle">Title Will Go Here</h3>
+		<div id="albums">
+			<p>Albums will go here</p>
+		</div>
+
+		<div id="output">Results go here</div>
+
+    
+	</body>
 </html>
